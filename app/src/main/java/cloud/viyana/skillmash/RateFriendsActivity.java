076d4805ab0b86d02ab23d7d516a1ffc75ccdaaa
rama41222 <dinushankanrg@gmail.com>
2018-04-11 21:50:33 +0530

    friend skill rating layouts and styles, floating buttons

---
+++ app/src/main/java/cloud/viyana/skillmash/RateFriendsActivity.java
@@ -0,0 +1,13 @@
+package cloud.viyana.skillmash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RateFriendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_friends);
+    }
+}
